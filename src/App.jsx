0cea863a5e0c6b81c233c2dc698e62a07b9a9cbd
ceejeey <muhammed030@gmail.com>
2022-10-24 13:45:45 +0530

    chores: check test

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,88 @@ import './App.css';
 function App() {
   const isApp = false;
   return (
-    <div className="App">
-      {/* <h1>Vite + React</h1>
-      <div className="card">
-        <p>hello</p>
-      </div> */}
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {isApp ? '' : <AppMirror />}
-    </div>
+    <>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+      <div className="App">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <p>hello</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        {isApp ? '' : <AppMirror />}
+      </div>
+    </>
   );
 }
 
